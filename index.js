0fcf94f351b392d9d48a68b15c98bb265a86a0bd
Priscila Moreno <priscila.moreno.wow@gmail.com>
2017-12-13 18:46:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function deleteFromObjectByKey(object, key){
   return newObject;
 }
 
-function deleteFromObjectByKey(object, key){
+function destructivelyDeleteFromObjectByKey(object, key){
   delete object.key;
   return object;
 }
